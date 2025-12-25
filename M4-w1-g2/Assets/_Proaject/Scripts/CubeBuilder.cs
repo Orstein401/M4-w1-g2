822dac1835ebc3be66c66223472c19dfbe3228ea
Orstein401 <alerom342@gmail.com>
2025-12-25 13:03:49 +0100

    ve1.2

--- M4-w1-g2/Assets/_Proaject/Scripts/CubeBuilder.cs
+++ M4-w1-g2/Assets/_Proaject/Scripts/CubeBuilder.cs
@@ -4,41 +4,13 @@ using UnityEngine;
 
 public class CubeBuilder : MonoBehaviour
 {
-    //[SerializeField] GameObject cuboPrefab;
-    ////righe colonne
-    //[SerializeField] private float rowLine;
-    //[SerializeField] private float colLine;
-    //[SerializeField] private float heightLine;
-
-    ////spazio tra le righe
-    //[SerializeField] private float spaceRow;
-    //[SerializeField] private float spaceCol;
-    //[SerializeField] private float spaceHeight;
-
-
-    //private void Start()
-    //{
-    //    for (int i = 0; i < rowLine; i++)
-    //    {
-    //        for (int j = 0; j < colLine; j++)
-    //        {
-    //            for (int k = 0; k < heightLine; k++)
-    //            {
-    //                GameObject panello = Instantiate(cuboPrefab, transform);
-    //                panello.transform.position = new Vector3(gameObject.transform.position.x + spaceRow * i, gameObject.transform.position.y + spaceHeight * k, gameObject.transform.position.z + spaceCol * j);
-    //            }
-    //        }
-    //    }
-
-    //}
 
     [SerializeField] private float SizeCube;
     [SerializeField] GameObject cuboPrefab;
 
 
-    [SerializeField] private float spaceRow;
-    [SerializeField] private float spaceCol;
-    [SerializeField] private float spaceHeight;
+    [SerializeField] private float space;
+
 
     private void Start()
     {
@@ -48,11 +20,11 @@ public class CubeBuilder : MonoBehaviour
             {
                 for (int k = 0; k < SizeCube; k++)
                 {
-                    if (k<1||i<1||k<1)
-                    {
-                        GameObject panello = Instantiate(cuboPrefab, transform);
-                        panello.transform.position = new Vector3(gameObject.transform.position.x + spaceRow * i, gameObject.transform.position.y + spaceHeight * k, gameObject.transform.position.z + spaceCol * j);
-                    }
+                            if (k<=0||k>=SizeCube-1||i==0||i>=SizeCube-1 || j == 0 || j >= SizeCube - 1)
+                            {
+                                GameObject panello = Instantiate(cuboPrefab, transform);
+                                panello.transform.position = new Vector3(gameObject.transform.position.x + space * i, gameObject.transform.position.y + space * k, gameObject.transform.position.z + space * j);
+                            }       
                    
                 }
             }
